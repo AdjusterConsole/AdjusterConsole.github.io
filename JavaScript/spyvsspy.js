56c5ac8f71a9d362904497fc2328a3982a8a376c
AdjusterConsole <mechprowilliams@gmail.com>
2024-07-03 03:03:37 -0500

    Update spyvsspy.js

--- JavaScript/spyvsspy.js
+++ JavaScript/spyvsspy.js
@@ -2,5 +2,5 @@ import { initializeFirebase } from './module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const api_Key = window.FIREBASE_API_KEY;
-  initializeFirebase(apiKey);
+  initializeFirebase(api_Key);
 });
